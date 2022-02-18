d41090576e0012aa125aa8ba212350e7e9031b94
aperminova-relay <aperminova@relaynetwork.com>
2022-02-17 20:36:35 -0500

    table, image binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -7,16 +7,30 @@ import {Component} from '@angular/core';
     selector: 'courses', //<courses></courses>
     template: `
     <h2>{{ title }}</h2>
+    <h2 [textContent]="title"></h2>
+
     <ul>
         <li *ngFor="let course of courses">
             {{ course }}
         </li>
     </ul>
+
+    <img src="{{ imageUrl }}"/>
+    <img [src]="imageUrl"/>
+
+    <table>
+        <tr>
+            <td [attr.colspan]="colSpan">{{tableContent}}</td>
+        </tr>
+    </table>
     `
 })
 export class CoursesComponent {
     title = "List of courses";
     courses;
+    colSpan = 2;
+    tableContent= 'Anna'
+    imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/The_Sun_by_the_Atmospheric_Imaging_Assembly_of_NASA%27s_Solar_Dynamics_Observatory_-_20100819.jpg/220px-The_Sun_by_the_Atmospheric_Imaging_Assembly_of_NASA%27s_Solar_Dynamics_Observatory_-_20100819.jpg';
 
     constructor(service: CoursesService) {  // instead of let service = new CoursesService();
         this.courses = service.getCourses();
